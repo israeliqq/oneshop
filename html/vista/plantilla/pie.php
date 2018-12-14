29bc7513f92dad29d5c1b70a9f6b65fc95c935c9
Israel <israeliqq@live.cl>
2018-12-13 23:40:22 -0300

    PHP PURO
    Se opta por aplicar html puro, ya que el framework no funciona correctamente.

---
+++ html/vista/plantilla/pie.php
@@ -0,0 +1,18 @@
+
+					
+
+<script src="tools/js/jquery-3.3.1.min.js"></script>
+<script src="tools/styles/bootstrap4/popper.js"></script>
+<script src="tools/styles/bootstrap4/bootstrap.min.js"></script>
+<script src="tools/plugins/greensock/TweenMax.min.js"></script>
+<script src="tools/plugins/greensock/TimelineMax.min.js"></script>
+<script src="tools/plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="tools/plugins/greensock/animation.gsap.min.js"></script>
+<script src="tools/plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="tools/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="tools/plugins/slick-1.8.0/slick.js"></script>
+<script src="tools/plugins/easing/easing.js"></script>
+<script src="tools/js/custom.js"></script>
+</body>
+
+</html>
